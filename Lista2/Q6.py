52add8e24f105f4880a81d3ccc5e3ac8ff01c4c1
oliveiraluizz <oliv.luizreis@gmail.com>
2022-04-26 20:03:03 -0300

    Correção Q6

--- Lista2/Q6.py
+++ Lista2/Q6.py
@@ -7,16 +7,17 @@ p=int(P)
 F = a + d + p
 
 F1 = (F*100)/110
+F1=int(F1)
 
 if F < 110:
     
-  if (F1 >= 0) and (F <= 50):
+  if (F1 >= 0) and (F1 <= 50):
       print("Seu pokemon nao fara progresso em batalhas")
-  elif (F1 >= 51) and (F <= 66):   
+  elif (F1 >= 51) and (F1 <= 66):   
       print("Seu pokemon esta acima da media")
-  elif (F1 >= 67) and (F <= 79):   
+  elif (F1 >= 67) and (F1 <= 79):   
       print("Seu pokemon certamente me chamou atencao")    
-  elif (F1 >= 80) and (F <= 100):   
+  elif (F1 >= 80) and (F1 <= 100):   
       print("Seu pokemon e uma maravilha")   
   
 else:
